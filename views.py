92a6e5683dbb86fcfc9a1d4f0d071258db0b741a
has3321 <144865642+has3321@users.noreply.github.com>
2024-11-12 21:08:36 +0500

    Add files via upload

---
+++ views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+# Create your views here.
+@api_view(['GET' , 'POST'])
+def books(request):
+    return Response('list of books' , status=status.HTTP_200_OK)
